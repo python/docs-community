e8df3df5e2f94c2a1d4e2e45a1f6ad217270ca5a
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-02-19 20:55:33 +0000

    Remove htmlhelp, man, epub settings

--- docs/conf.py
+++ docs/conf.py
@@ -32,22 +32,3 @@ html_theme = 'furo'
 # relative paths to custom static files
 html_static_path = ['_static']
 
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'Docscommunitydoc'
-
-
-# Options for manual page output
-man_pages = [
-    (master_doc, 'docscommunity', 'Docs Community Documentation',
-     [author], 1)
-]
-
-
-# Bibliographic Dublin Core info.
-epub_title = project
-epub_author = author
-epub_publisher = author
-epub_copyright = copyright
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
