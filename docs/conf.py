5fe986705c00f4cdb8f749c5ad71d88bc9a889ce
Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>
2025-04-02 07:36:17 +0100

    Update docs/conf.py
    Co-authored-by: Carol Willing <carolcode@willingconsulting.com>

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ intersphinx_disabled_reftypes = []
 # The theme to use for HTML pages
 html_theme = "furo"
 
-html_title = "Python Documentation Community"
+html_title = "Python Docs Community Group"
 
 # Options for the linkcheck builder
 # =================================
